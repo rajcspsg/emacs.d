ada6724f7df16faf8ce3d26e912a867e8d24521b
RAJKUMAR NATARAJAN <raj.cs.psg@gmail.com>
2021-03-09 19:27:54 -0500

    Update golang.el

--- golang.el
+++ golang.el
@@ -1,51 +1,46 @@
-(use-package lsp-mode
-  :ensure t
-  :commands (lsp lsp-deferred)
-  :hook (go-mode . lsp-deferred))
+(use-package go-mode
+  :mode "\\.go\\'"
+  :hook (before-save . gofmt-before-save))
 
-;;Set up before-save hooks to format buffer and add/delete imports.
-;;Make sure you don't have other gofmt/goimports hooks enabled.
+(require 'flycheck)
+(global-flycheck-mode 1)
+
+
+(add-hook 'before-save-hook 'gofmt-before-save)
+
+(use-package lsp-mode
+  :defer t
+  :commands (lsp lsp-deferred)
+  :hook (go-mode . lsp-deferred)
+  :init
+  (setq lsp-auto-guess-root t)       ; Detect project root
+  ;; disable Yasnippet
+  (setq lsp-enable-snippet nil)
+  (setq lsp-prefer-flymake nil)      ; Use lsp-ui and flycheck
+  (setq flymake-fringe-indicator-position 'right-fringe)
+  )
+
+(use-package flycheck-golangci-lint
+  :ensure t)
+
+(use-package dap-mode
+  :config
+  (dap-mode 1)
+  (setq dap-print-io t)
+  ;;(setq fit-window-to-buffer-horizontally t)
+  ;;(setq window-resize-pixelwise t)
+  (require 'dap-hydra)
+  (require 'dap-go)
+  (dap-go-setup)
+  (use-package dap-ui
+    :ensure nil
+    :config
+    (dap-ui-mode 1)
+    )
+  )
 
 (defun lsp-go-install-save-hooks ()
   (add-hook 'before-save-hook #'lsp-format-buffer t t)
   (add-hook 'before-save-hook #'lsp-organize-imports t t))
+
 (add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
-
-;;Optional - provides fancier overlays.
-
-(use-package lsp-ui
-  :ensure t
-  :commands lsp-ui-mode
-  :init
-)
-
-;;Company mode is a standard completion package that works well with lsp-mode.
-;;company-lsp integrates company mode completion with lsp-mode.
-;;completion-at-point also works out of the box but doesn't support snippets.
-
-(use-package company
-  :ensure t
-  :config
-  (setq company-idle-delay 0)
-  (setq company-minimum-prefix-length 1))
-
-(use-package company-lsp
-  :ensure t
-  :commands company-lsp)
-
-;;Optional - provides snippet support.
-
-(use-package yasnippet
-  :ensure t
-  :commands yas-minor-mode
-  :hook (go-mode . yas-minor-mode))
-
-;;lsp-ui-doc-enable is false because I don't like the popover that shows up on the right
-;;I'll change it if I want it back
-
-
-(setq lsp-ui-doc-enable nil
-      lsp-ui-peek-enable t
-      lsp-ui-sideline-enable t
-      lsp-ui-imenu-enable t
-      lsp-ui-flycheck-enable t)
